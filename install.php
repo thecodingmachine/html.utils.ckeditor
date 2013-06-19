293028df8c0ad7c2fbf01ebf47efa5095a530118
Kevin Nguyen <k.nguyen@thecodingmachine.com>
2013-06-19 16:05:30 +0200

    Initial Commit
    Signed-off-by: Kevin Nguyen <k.nguyen@thecodingmachine.com>

---
+++ install.php
@@ -0,0 +1,24 @@
+<?php
+use Mouf\Html\Utils\WebLibraryManager\WebLibraryInstaller;
+use Mouf\Actions\InstallUtils;
+use Mouf\MoufManager;
+
+require_once __DIR__."/../../autoload.php";
+
+// Let's init Mouf
+InstallUtils::init(InstallUtils::$INIT_APP);
+
+// Let's create the instance
+$moufManager = MoufManager::getMoufManager();
+
+WebLibraryInstaller::installLibrary("javascript.bootstrap",
+	array(
+		'vendor/mouf/html.utils.ckeditor/ckeditor/ckeditor.js'
+	)
+);
+
+// Let's rewrite the MoufComponents.php file to save the component
+$moufManager->rewriteMouf();
+
+// Finally, let's continue the install
+InstallUtils::continueInstall();
